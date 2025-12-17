81db9051237eaa913e644532baa2a3eb300c5145
Daniyar008 <daniyarsyltangereev@gmail.com>
2025-12-17 14:37:48 +0500

    Add babel to requirements and compile translations in build.sh

--- ecomprj/build.sh
+++ ecomprj/build.sh
@@ -8,6 +8,25 @@ echo "====== Installing dependencies ======"
 pip install --upgrade pip
 pip install -r requirements.txt
 
+echo "====== Compiling translation files ======"
+python -c "
+from babel.messages.mofile import write_mo
+from babel.messages.pofile import read_po
+import os
+
+langs = ['en', 'kk', 'ru']
+for lang in langs:
+    po_path = f'locale/{lang}/LC_MESSAGES/django.po'
+    mo_path = f'locale/{lang}/LC_MESSAGES/django.mo'
+    if os.path.exists(po_path):
+        with open(po_path, 'r', encoding='utf-8') as po_file:
+            catalog = read_po(po_file)
+        with open(mo_path, 'wb') as mo_file:
+            write_mo(mo_file, catalog)
+        print(f'Compiled {po_path} -> {mo_path}')
+print('Translation files compiled successfully!')
+"
+
 echo "====== Collecting static files ======"
 python manage.py collectstatic --no-input
 
